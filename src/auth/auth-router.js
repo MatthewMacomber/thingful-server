de2e5763bb3f9d3cf479d1137f07b6a82ba4b5cd
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-08-28 08:47:24 -0700

    Changed from basic authorization to jwt authorization and added registration handling

---
+++ src/auth/auth-router.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const AuthService = require('./auth-service');
+
+const authRouter = express.Router();
+const parseBody = express.json();
+
+authRouter
+  .post('/login', parseBody, (req, res, next) => {
+    const {user_name, password} = req.body;
+    const loginUser = {user_name, password};
+
+    for (const [key, value] of Object.entries(loginUser)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        });
+      };
+    };
+    
+    AuthService.getUserWithUserName(req.app.get('db'), loginUser.user_name)
+      .then(dbUser => {
+        if (!dbUser) {
+          return res.status(400).json({
+            error: 'Incorrect user name or password'
+          });
+        };
+        return AuthService.comparePasswords(loginUser, dbUser.password)
+          .then(compare=> {
+            if (!compare) {
+              return res.status(400).json({
+                error: 'Incorrect user name or password'
+              });
+            };
+            const sub = dbUser.user_name;
+            const payload = {user_id: dbUser.user_id};
+            res.send({authToken: AuthService.createJwt(sub, payload)});
+          });
+      })
+      .catch(next);
+  })
+
+  module.exports = authRouter;
